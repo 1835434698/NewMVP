35ed448663b80254d7f95bdfb5d03d5adc0549ae
tzy <zhaoyangtang@jieyuechina.com>
2019-01-29 12:21:35 +0800

    Android自定义view 实现圆的扩散(

--- app/src/main/java/com/tangzy/tzymvp/fragment/SecondFragment.java
+++ app/src/main/java/com/tangzy/tzymvp/fragment/SecondFragment.java
@@ -1,11 +1,18 @@
 package com.tangzy.tzymvp.fragment;
 
+import android.view.View;
+
 import com.tangzy.themvp.databind.DataBinder;
+import com.tangzy.tzymvp.R;
 import com.tangzy.tzymvp.databind.FragmentActivityBinder;
 import com.tangzy.tzymvp.fragment.base.BaseFragment;
+import com.tangzy.tzymvp.listener.NoDoubleClickListener;
+import com.tangzy.tzymvp.view.DiffuseView;
 import com.tangzy.tzymvp.viewbind.SecondFragDelegate;
 
 public class SecondFragment extends BaseFragment<SecondFragDelegate> {
+
+    private DiffuseView diffuseView;
     @Override
     protected Class<SecondFragDelegate> getDelegateClass() {
         return SecondFragDelegate.class;
@@ -15,4 +22,27 @@ public class SecondFragment extends BaseFragment<SecondFragDelegate> {
     public DataBinder getDataBinder() {
         return new FragmentActivityBinder();
     }
+
+    @Override
+    protected void bindEvenListener() {
+        super.bindEvenListener();
+        viewDelegate.get(R.id.button).setOnClickListener(noDoubleClickListener);
+        viewDelegate.get(R.id.button2).setOnClickListener(noDoubleClickListener);
+        diffuseView = viewDelegate.get(R.id.diffuseView);
+    }
+
+
+    NoDoubleClickListener noDoubleClickListener = new NoDoubleClickListener() {
+        @Override
+        public void onNoDoubleClick(View v) {
+            switch (v.getId()){
+                case R.id.button:
+                    diffuseView.start();
+                    break;
+                case R.id.button2:
+                    diffuseView.stop();
+                    break;
+            }
+        }
+    };
 }
