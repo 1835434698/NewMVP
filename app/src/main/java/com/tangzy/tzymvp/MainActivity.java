7411d5864a1b64e813337e80578f52c0cd82382d
tzy <zhaoyangtang@jieyuechina.com>
2019-12-18 09:54:44 +0800

    hook

--- app/src/main/java/com/tangzy/tzymvp/MainActivity.java
+++ app/src/main/java/com/tangzy/tzymvp/MainActivity.java
@@ -36,6 +36,7 @@ import com.tangzy.tzymvp.annotation.Test;
 import com.tangzy.tzymvp.bean.DataBean;
 import com.tangzy.tzymvp.bean.TzyBean;
 import com.tangzy.tzymvp.bean.UserBean;
+import com.tangzy.tzymvp.hook.HookSetOnClickListenerHelper;
 import com.tangzy.tzymvp.test.ChredUser;
 import com.tangzy.tzymvp.test.DynamicProxy;
 import com.tangzy.tzymvp.test.Iuser;
@@ -406,4 +407,72 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
+    private static Integer count = 0;
+    private static final Integer FULL = 10;
+    private static String LOCK = "lock";
+    public void producerConsumer(View view) {
+        new Thread(new Producer()).start();
+        new Thread(new Consumer()).start();
+        new Thread(new Producer()).start();
+        new Thread(new Consumer()).start();
+        new Thread(new Producer()).start();
+        new Thread(new Consumer()).start();
+        new Thread(new Producer()).start();
+        new Thread(new Consumer()).start();
+
+    }
+
+    public void hook(View view) {
+        HookSetOnClickListenerHelper.hook(this, view);
+
+    }
+
+    class Producer implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Thread.sleep(3000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                synchronized (LOCK) {
+                    while (count == FULL) {
+                        try {
+                            LOCK.wait();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    count++;
+                    System.out.println(Thread.currentThread().getName() + "生产者生产，目前总共有" + count);
+                    LOCK.notifyAll();
+                }
+            }
+        }
+    }
+
+    class Consumer implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (LOCK) {
+                    while (count == 0) {
+                        try {
+                            LOCK.wait();
+                        } catch (Exception e) {
+                        }
+                    }
+                    count--;
+                    System.out.println(Thread.currentThread().getName() + "消费者消费，目前总共有" + count);
+                    LOCK.notifyAll();
+                }
+            }
+        }
+    }
 }
