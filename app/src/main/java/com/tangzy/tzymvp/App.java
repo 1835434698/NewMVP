bfbb0343163cb44d4c5215e8bc473619b16d550c
tzy <zhaoyangtang@jieyuechina.com>
2019-12-19 15:57:24 +0800

    判读app是否在前台

--- app/src/main/java/com/tangzy/tzymvp/App.java
+++ app/src/main/java/com/tangzy/tzymvp/App.java
@@ -1,7 +1,13 @@
 package com.tangzy.tzymvp;
 
+import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.iflytek.cloud.SpeechConstant;
@@ -9,6 +15,7 @@ import com.iflytek.cloud.SpeechUtility;
 import com.tangzy.tzymvp.util.Logger;
 
 public class App extends Application {
+    public int count = 0;
     @Override
     public void onCreate() {
         super.onCreate();
@@ -19,6 +26,54 @@ public class App extends Application {
             ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
         }
         ARouter.init(this); // 尽可能早，推荐在Application中初始化
+
+
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+                Log.v("viclee", activity + "onActivityStopped");
+                count--;
+                if (count == 0) {
+                    Log.v("viclee", ">>>>>>>>>>>>>>>>>>>切到后台  lifecycle");
+                }
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+                Log.v("viclee", activity + "onActivityStarted");
+                if (count == 0) {
+                    Log.v("viclee", ">>>>>>>>>>>>>>>>>>>切到前台  lifecycle");
+                }
+                count++;
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+                Log.v("viclee", activity + "onActivitySaveInstanceState");
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+                Log.v("viclee", activity + "onActivityResumed");
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+                Log.v("viclee", activity + "onActivityPaused");
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+                Log.v("viclee", activity + "onActivityDestroyed");
+            }
+
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+                Log.v("viclee", activity + "onActivityCreated");
+            }
+        });
+
     }
 
     @Override
@@ -26,4 +81,8 @@ public class App extends Application {
         Logger.d("tangzy", "attachBaseContext");
         super.attachBaseContext(base);
     }
+
+
+
+
 }
