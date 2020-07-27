cec598810973438108175e0261c6b0aa251a5505
tangzhaoyang <tangzhaoyang>
2020-07-27 19:34:50 +0800

    Revert "Retrift gson自主解析问题、pdf预览"
    This reverts commit b66420f7

--- app/src/main/java/com/tangzy/tzymvp/net/api/APIService.java
+++ app/src/main/java/com/tangzy/tzymvp/net/api/APIService.java
@@ -1,6 +1,5 @@
 package com.tangzy.tzymvp.net.api;
 
-import com.tangzy.tzymvp.bean.base.BaseResponse;
 import com.tangzy.tzymvp.net.bean.ListBean;
 import com.tangzy.tzymvp.net.bean.ResultBean;
 import com.tangzy.tzymvp.net.bean.TestBean;
@@ -9,13 +8,9 @@ import java.util.List;
 import java.util.Map;
 
 import io.reactivex.Observable;
-import okhttp3.ResponseBody;
 import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
-import retrofit2.http.GET;
-import retrofit2.http.Header;
 import retrofit2.http.POST;
-import retrofit2.http.Query;
 
 /**
  * class
@@ -58,17 +53,5 @@ public interface APIService {
     @FormUrlEncoded
     @POST("test/getList2.php")
     Observable<List<TestBean>> getList2(@FieldMap Map<String, String> paramsMap);
-
-    /**
-     * 获得粉丝的客户列表接口服务
-     *
-     * @param queryJson
-     * @param baseUrlType
-     * @return
-     */
-    @GET("customer/follow/fans/v2/getMapList")
-    Observable<ResponseBody> getFollowFansMapList(
-            @Query(value = "queryJson", encoded = true)String queryJson);
-
 
 }
